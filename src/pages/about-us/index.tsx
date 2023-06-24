094ddcb836c1fa3c3fca3e1281252ea1d5ef7d3d
prapapan <prapapan.pila99@gmail.com>
2023-06-24 23:42:19 +0700

    fix : update menu

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,24 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaLine } from 'react-icons/fa';
 
 import Layout from '@/components/layout/Layout';
-
-import EmailLink from '@/pages/email-link';
 
 const AboutUs = () => {
   return (
     <Layout backgroundImage='/images/bgn.png'>
       <div className=' h-screen  pt-40'>
-        <div className=' mt-4 flex flex-col text-center   '>
-          <div className='mb-10 text-4xl  font-semibold text-[#ffcc00]'>
-            ติดต่อเรา
-          </div>
-
-          <EmailLink email='deschooling.thaipbs@gmail.com'>
-            <div className='text-3xl text-white'>
-              deschooling.thaipbs@gmail.com
-            </div>
-          </EmailLink>
+        <div className='mt-4 flex justify-center text-4xl font-semibold text-[#ffcc00]'>
+          ติดต่อเรา
         </div>
+
+        <div className=' mt-10 grid grid-cols-5'>
+          <div className='col-start-2 justify-self-end pr-10'>
+            <AiOutlineMail size={50} className=' text-white' />
+          </div>
+          <div className=' col-start-3 col-end-6  '>
+            <Link href='mailto:deschooling.thaipbs@gmail.com'>
+              <div className=' text-3xl text-white '>
+                deschooling.thaipbs@gmail.com
+              </div>
+            </Link>
+          </div>
+        </div>
+
+        <div className='mt-4 grid grid-cols-5 justify-items-stretch'>
+          <div className='col-start-2 justify-self-end pr-10'>
+            <FaLine size={50} className=' text-white' />
+          </div>
+          <div className=' col-start-3 col-end-6 '>
+            <Image
+              className='rounded-md bg-[#ffcc00] p-2'
+              src='/images/barcode.png'
+              alt='Picture of the author'
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+
+        {/* <div className='text-white'>
+            <GrFacebook size={40} />
+          </div> */}
       </div>
     </Layout>
   );

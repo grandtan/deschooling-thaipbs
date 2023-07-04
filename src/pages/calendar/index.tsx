78b67a49ac7496ef11822890a4d7c1c8e1998ec0
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-04 23:02:42 +0700

    fix:update navbar

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,16 +1,33 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import React from 'react';
-
-import Layout from '@/components/layout/Layout';
 
 const Calendar = () => {
   return (
-    <Layout backgroundImage='/images/bg-activity2.png'>
-      <div className=' h-screen  pt-40'>
-        <div className=' mt-4 flex justify-center text-4xl font-semibold text-[#ffcc00]'>
-          ปฏิทินกิจกรรม
-        </div>
-      </div>
-    </Layout>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 

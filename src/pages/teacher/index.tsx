b266c6283b8230bd2ea843f769a94d0f1f1a57e3
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-06 15:38:26 +0700

    fix : create display VDO

--- src/pages/teacher/index.tsx
+++ src/pages/teacher/index.tsx
@@ -19,31 +19,30 @@ const Teacher = () => {
 
   return (
     <Layout backgroundImage='/images/bgn.png'>
-      <div className=' h-screen  pt-40'>
+      <div className=' '>
         {/* <div
         className=' pt-40'
         style={{
           backgroundImage: `url(${backgroundImage})`,
         }}
       > */}
-        <div className=' mt-4 flex justify-center text-4xl font-semibold text-[#ffcc00]'>
+        <div className=' mt-36 flex justify-center text-3xl font-semibold text-[#ffcc00]'>
           Learning Space แบ่งปันนวัตกรรมการสอน
         </div>
-        <div className=' my-10 flex flex-row'>
-          <div className='w-1/5 text-white'>picture</div>
-          <div className='mb-10 w-4/5 pr-10'>
-            <Grid container spacing={2}>
-              {link.map((e) => (
-                <Grid item xs={12} md={6} lg={3} key={e}>
-                  <Link href={e} key={e}>
-                    <div className=' h-60  bg-slate-50 p-1 hover:bg-[#ffcc00]'>
-                      <div className=' h-full  bg-black text-white'>VDO</div>
+        <div className=' mx-20 mt-10 flex flex-row justify-center'>
+          <Grid container spacing={4}>
+            {link.map((e) => (
+              <Grid item xs={12} md={6} lg={3} key={e}>
+                <Link href={e} key={e}>
+                  <div className='aspect-video  w-full rounded-t-lg rounded-bl-lg bg-white  hover:-translate-y-4   '>
+                    <div className=' h-64 rounded-t-lg rounded-bl-lg hover:bg-[#ffcc00] '>
+                      VDO
                     </div>
-                  </Link>
-                </Grid>
-              ))}
-            </Grid>
-          </div>
+                  </div>
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </div>
     </Layout>

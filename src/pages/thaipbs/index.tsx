2a2023275df43a9dd63e8d938bd93031bf2d3e2e
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-20 23:05:23 +0700

    fix : update bg ThaiPbs page

--- src/pages/thaipbs/index.tsx
+++ src/pages/thaipbs/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import Layout from '@/components/layout/Layout';
-import { PiPencilLine } from 'react-icons/pi';
+import { TfiRulerPencil } from 'react-icons/tfi';
 
 const ThaiPbs = () => {
   return (
     <Layout backgroundImage='/images/bg-master.png'>
       <div className=' h-screen py-10'>
-        <div className=' flex items-center justify-center font-semibold text-[#ffba00]'>
-          <PiPencilLine size={30} />
+        <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
+          <TfiRulerPencil size={30} />
           <div className=' text-2xl '>ความหมายของ Deschooling</div>
         </div>
       </div>

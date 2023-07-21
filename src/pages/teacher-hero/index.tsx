6e0ef9268de8b7f04f36d4d5f1937792925962d5
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-21 10:46:37 +0700

    fix : update bg teacher-hero/deschooling

--- src/pages/teacher-hero/index.tsx
+++ src/pages/teacher-hero/index.tsx
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import { PiPencilLine } from 'react-icons/pi';
+import { PiPencilLine, PiTelevisionFill } from 'react-icons/pi';
 import Layout from '@/components/layout/Layout';
 import { YoutubeResponse } from '@/types/teacher';
 
@@ -33,39 +33,12 @@ const TeacherHero = () => {
 
   return (
     <Layout>
-      <div className='mx-20 py-10'>
-        <div className='flex justify-center space-x-4 text-2xl font-semibold text-[#ffba00]'>
-          <PiPencilLine size={30} />
-          <div>Teacher Hero</div>
-        </div>
-        <div className='mt-10 flex flex-row justify-center'>
-          <Grid container spacing={4}>
-            {itemYoutube?.etag &&
-              itemYoutube.items.map((e, i) => (
-                <Grid item xs={12} md={6} lg={3} key={i}>
-                  <Link
-                    href={`https://www.youtube.com/watch?v=${e.snippet.resourceId.videoId}`}
-                    passHref
-                    target='_blank'
-                  >
-                    <div className='aspect-video w-full  rounded-t-xl  rounded-bl-xl bg-white p-1.5  duration-500 hover:-translate-y-6  hover:bg-[#ffcc00] hover:text-white hover:ease-in'>
-                      <div>
-                        <img
-                          className='rounded-t-xl rounded-bl-xl'
-                          src={e.snippet.thumbnails.high.url}
-                          width='100%'
-                          height='100%'
-                          alt={e.snippet.title}
-                        />
-                      </div>
-                      <div className='h-24 p-4 text-left '>
-                        {e.snippet.title}
-                      </div>
-                    </div>
-                  </Link>
-                </Grid>
-              ))}
-          </Grid>
+      <div className='mx-24 h-screen py-10'>
+        <div className='flex justify-between'>
+          <div className=' flex items-center space-x-4 font-semibold text-[#ffba00]'>
+            <PiTelevisionFill size={35} />
+            <div className=' text-2xl '>Teacher Hero</div>
+          </div>
         </div>
       </div>
     </Layout>

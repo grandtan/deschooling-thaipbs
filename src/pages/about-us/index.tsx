cb11f366adca7f9149b13cffec15869ef0c63bbe
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-21 11:50:30 +0700

    fix :update aboutUs page

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -5,46 +5,37 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { FaLine } from 'react-icons/fa';
 
 import Layout from '@/components/layout/Layout';
+import { RiMailUnreadFill } from 'react-icons/ri';
+import { Stack } from '@mui/material';
+import Item from 'antd/es/descriptions/Item';
 
 const AboutUs = () => {
   return (
-    <Layout backgroundImage='/images/bgn.png'>
-      <div className=''>
-        <div className='flex justify-center text-4xl font-semibold text-[#ffcc00]'>
-          ติดต่อเรา
+    <Layout backgroundImage='/images/bg-master.png'>
+      <div className=' mx-24 h-screen  py-10'>
+        <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
+          <RiMailUnreadFill size={35} />
+          <div className=' text-2xl '>ติดต่อเรา</div>
         </div>
 
-        <div className=' grid grid-cols-5'>
-          <div className='col-start-2 justify-self-end pr-10'>
-            <AiOutlineMail size={50} className=' text-white' />
+        <div className=' flex flex-row justify-center space-x-36'>
+          <div className='  mt-24 flex flex-col items-center  justify-center space-y-10'>
+            <div className=' text-2xl  text-[#ffba00]'>Email</div>
+            <div className=' rounded-full  border-2 bg-white p-3 text-2xl hover:text-red-500'>
+              <Link href='mailto:deschooling.thaipbs@gmail.com'>
+                <RiMailUnreadFill size={50} />
+              </Link>
+            </div>
           </div>
-          <div className=' col-start-3 col-end-6  '>
-            <Link href='mailto:deschooling.thaipbs@gmail.com'>
-              <div className=' text-3xl text-white '>
-                deschooling.thaipbs@gmail.com
-              </div>
-            </Link>
-          </div>
-        </div>
-
-        <div className='mt-4 grid grid-cols-5 justify-items-stretch'>
-          <div className='col-start-2 justify-self-end pr-10'>
-            <FaLine size={50} className=' text-white' />
-          </div>
-          <div className=' col-start-3 col-end-6 '>
-            <Image
-              className='rounded-md bg-[#ffcc00] p-2'
-              src='/images/barcode.png'
-              alt='Picture of the author'
-              width={300}
-              height={300}
-            />
+          <div className='  mt-24 flex flex-col items-center  justify-center space-y-10'>
+            <div className=' text-2xl  text-[#ffba00]'>Line</div>
+            <div className=' rounded-full  border-2 bg-white p-3 text-2xl hover:text-green-500'>
+              <Link href='mailto:deschooling.thaipbs@gmail.com'>
+                <FaLine size={50} />
+              </Link>
+            </div>
           </div>
         </div>
-
-        {/* <div className='text-white'>
-            <GrFacebook size={40} />
-          </div> */}
       </div>
     </Layout>
   );

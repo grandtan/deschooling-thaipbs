6c64cbfaeff1ac8b81def5a3cd436338f8775ca7
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-21 14:13:06 +0700

    fix : AboutUs page

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -11,28 +11,40 @@ import Item from 'antd/es/descriptions/Item';
 
 const AboutUs = () => {
   return (
-    <Layout backgroundImage='/images/bg-master.png'>
+    <Layout container={false}>
       <div className=' mx-24 h-screen  py-10'>
         <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
           <RiMailUnreadFill size={35} />
-          <div className=' text-2xl '>ติดต่อเรา</div>
+          <div className=' text-4xl '>ติดต่อเรา</div>
         </div>
 
-        <div className=' flex flex-row justify-center space-x-36'>
-          <div className='  mt-24 flex flex-col items-center  justify-center space-y-10'>
-            <div className=' text-2xl  text-[#ffba00]'>Email</div>
-            <div className=' rounded-full  border-2 bg-white p-3 text-2xl hover:text-red-500'>
-              <Link href='mailto:deschooling.thaipbs@gmail.com'>
-                <RiMailUnreadFill size={50} />
-              </Link>
+        <div
+          className='px-4'
+          style={{
+            background: `url('/images/bg-aboutus.png')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <div className=' flex flex-row justify-center space-x-36'>
+            <div className='  mt-52 flex flex-col items-center  justify-center space-y-4'>
+              <div className=' text-3xl font-bold'>Email</div>
+              <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:text-red-500 '>
+                <Link href='mailto:deschooling.thaipbs@gmail.com'>
+                  <RiMailUnreadFill size={50} />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className='  mt-24 flex flex-col items-center  justify-center space-y-10'>
-            <div className=' text-2xl  text-[#ffba00]'>Line</div>
-            <div className=' rounded-full  border-2 bg-white p-3 text-2xl hover:text-green-500'>
-              <Link href='mailto:deschooling.thaipbs@gmail.com'>
-                <FaLine size={50} />
-              </Link>
+            <div className=' mt-52 flex flex-col items-center  justify-center space-y-4'>
+              <div className=' text-3xl font-bold'>Line</div>
+              <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:text-green-500 '>
+                <Link href='mailto:deschooling.thaipbs@gmail.com'>
+                  <FaLine size={50} />
+                </Link>
+              </div>
             </div>
           </div>
         </div>

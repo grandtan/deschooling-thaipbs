3f8ee96ade133b870291fc13edae3e4e79de9c45
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-21 17:46:34 +0700

    update AboutUs page

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -13,39 +13,22 @@ const AboutUs = () => {
   return (
     <Layout container={false}>
       <div className=' mx-24 h-screen  py-10'>
-        <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
+        <div className=' flex items-center space-x-4 font-semibold text-[#ffba00]'>
           <RiMailUnreadFill size={35} />
           <div className=' text-3xl '>ติดต่อเรา</div>
         </div>
 
-        <div
-          className='px-4'
-          style={{
-            background: `url('/images/bg-aboutus.png')`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          <div className=' flex flex-row justify-center space-x-36'>
-            <div className='  mt-52 flex flex-col items-center  justify-center space-y-4'>
-              <div className=' text-3xl font-bold'>Email</div>
-              <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:text-red-500 '>
-                <Link href='mailto:deschooling.thaipbs@gmail.com'>
-                  <RiMailUnreadFill size={50} />
-                </Link>
-              </div>
-            </div>
-            <div className=' mt-52 flex flex-col items-center  justify-center space-y-4'>
-              <div className=' text-3xl font-bold'>Line</div>
-              <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:text-green-500 '>
-                <Link href='mailto:deschooling.thaipbs@gmail.com'>
-                  <FaLine size={50} />
-                </Link>
-              </div>
-            </div>
+        <div className=' mt-64 flex flex-row items-center justify-center space-x-28 '>
+          <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
+            <Link href='mailto:deschooling.thaipbs@gmail.com'>
+              <RiMailUnreadFill size={50} />
+            </Link>
+          </div>
+
+          <div className='rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
+            <Link href='mailto:deschooling.thaipbs@gmail.com'>
+              <FaLine size={50} />
+            </Link>
           </div>
         </div>
       </div>

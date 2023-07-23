e9f66b12903f45859b3cbcd2244b675911a60784
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-23 21:25:15 +0700

    fix : create name icon

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -48,38 +48,56 @@ const Home = () => {
             }}
           >
             <div className=' relative h-full w-full'>
-              <div className='absolute inset-x-0 bottom-0 grid grid-cols-7 gap-x-1 '>
+              <div className=' absolute inset-x-0 bottom-0 grid grid-cols-7 '>
                 <div className='absolute bottom-0 left-0 col-start-1 '>
+                  <div className='absolute left-[110px] top-[110px] text-xl text-white '>
+                    เด็กทารก
+                  </div>
                   <BabyHome
-                    className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
+                    className='cursor-pointer  duration-700 hover:-translate-y-4   hover:ease-in-out'
                     onClick={() => router.push('/baby')}
                   />
                 </div>
                 <div className='absolute  bottom-0 col-start-2 '>
+                  <div className='absolute left-[110px] top-[-10px] text-xl text-white '>
+                    นักเรียน
+                  </div>
                   <BabyHome2
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/student')}
                   />
                 </div>
                 <div className='absolute  bottom-4 col-start-3 '>
+                  <div className='absolute left-[110px] top-[-30px] text-xl text-white '>
+                    รัฐบาล
+                  </div>
                   <GirlHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/government')}
                   />
                 </div>
                 <div className='absolute bottom-2 col-start-4'>
+                  <div className='absolute left-[110px] top-[240px] text-xl text-white '>
+                    ครูและผู้อำนวยการ
+                  </div>
                   <TeacherHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/teacher-all')}
                   />
                 </div>
                 <div className='absolute bottom-0 col-start-5 pl-10'>
+                  <div className='absolute left-[110px] top-[-20px] text-xl text-white '>
+                    พ่อแม่และผู้ปกครอง
+                  </div>
                   <ParentHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/parent-all')}
                   />
                 </div>
                 <div className='absolute bottom-0 left-10  col-start-6 '>
+                  <div className='absolute left-[100px] top-[350px] text-xl text-white '>
+                    หน่วยงานท้องถิ่น
+                  </div>
                   <LocalGovernmentHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/local-government')}

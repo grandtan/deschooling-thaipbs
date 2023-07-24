cb06d3d5ecb3f2c2bd6a9af0896fd469e25459b7
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-24 18:27:44 +0700

    fix : create bg

--- src/pages/activity-register/index.tsx
+++ src/pages/activity-register/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import Layout from '@/components/layout/Layout';
 
 import { BsFillCalendarHeartFill } from 'react-icons/bs';
+import Link from 'next/link';
+import { MdComputer } from 'react-icons/md';
+import { FaPodcast } from 'react-icons/fa';
 
 const ActivityRegister = () => {
   return (
-    <Layout backgroundImage='/images/bg-master.png'>
+    <Layout backgroundImage='/images/bg-activity-register.png'>
       <div className='mx-24 h-screen py-10'>
         <div className=' flex flex-row items-center space-x-4 font-semibold text-[#ffba00]'>
           <BsFillCalendarHeartFill size={35} />

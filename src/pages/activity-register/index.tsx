62ce3f7f13009b440d8461a8f7934a2c89415a77
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-24 18:30:33 +0700

    fix : update new bg

--- src/pages/activity-register/index.tsx
+++ src/pages/activity-register/index.tsx
@@ -9,7 +9,7 @@ import { FaPodcast } from 'react-icons/fa';
 
 const ActivityRegister = () => {
   return (
-    <Layout backgroundImage='/images/bg-activity-register.png'>
+    <Layout backgroundImage='/images/bg-master.png'>
       <div className='mx-24 h-screen py-10'>
         <div className=' flex flex-row items-center space-x-4 font-semibold text-[#ffba00]'>
           <BsFillCalendarHeartFill size={35} />

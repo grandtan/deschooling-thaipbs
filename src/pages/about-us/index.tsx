58cc1c8bbd544e705c39c65dc9a0ac77ec3010d2
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-24 18:53:08 +0700

    fix : udate font size

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { FaLine } from 'react-icons/fa';
+import { BiQrScan } from 'react-icons/bi';
 
 import Layout from '@/components/layout/Layout';
 import { RiMailUnreadFill } from 'react-icons/ri';
@@ -11,28 +12,44 @@ import Item from 'antd/es/descriptions/Item';
 
 const AboutUs = () => {
   return (
-    <Layout container={false}>
+    <Layout backgroundImage='/images/bg-activity-register.png'>
       <div className=' mx-24 h-screen  py-10'>
         <div className=' flex items-center space-x-4 font-semibold text-[#ffba00]'>
           <RiMailUnreadFill size={35} />
           <div className=' text-3xl '>ติดต่อเรา</div>
         </div>
 
-        <div className=' mt-64 flex flex-row items-center justify-center space-x-28 '>
-          <div className=' rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
-            <Link href='mailto:deschooling.thaipbs@gmail.com'>
-              <RiMailUnreadFill size={50} />
-            </Link>
+        <div className='mt-20 flex  flex-row items-center justify-center space-x-28 '>
+          <div className='flex flex-col space-y-6'>
+            <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
+              EMAIL
+            </div>
+            <div className=' rounded-full  border-2 border-black bg-white p-6 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
+              <Link href='mailto:deschooling.thaipbs@gmail.com'>
+                <RiMailUnreadFill size={100} />
+              </Link>
+            </div>
           </div>
 
-          <div className='rounded-full  border-2 border-black bg-white p-3 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
-            <Link
-              href='https://line.me/R/ti/p/@deschooling?from=page'
-              passHref
-              target='_blank'
-            >
-              <FaLine size={50} />
-            </Link>
+          <div className=' flex-col space-y-6'>
+            <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
+              LINE
+            </div>
+            <div className='rounded-full border-2 border-black bg-white p-6 text-2xl hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white'>
+              <Link
+                href='https://line.me/R/ti/p/@deschooling?from=page'
+                passHref
+                target='_blank'
+              >
+                <FaLine size={100} />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-10 flex  flex-row items-center justify-center space-x-28 '>
+          <div className='text-center font-mono text-2xl  text-white'>
+            Compose & Scan Me !
           </div>
         </div>
       </div>

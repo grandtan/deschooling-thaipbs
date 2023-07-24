90583e97f0331295475bb0e31eed129b870c53c9
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-07-24 16:16:43 +0700

    fix : create new bg

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -10,23 +10,23 @@ import { Grid } from '@mui/material';
 import Item from 'antd/es/descriptions/Item';
 const Student = () => {
   return (
-    <Layout backgroundImage='/images/bg-master.png'>
+    <Layout backgroundImage='/images/bg-student.png'>
       <div className=' mx-24 h-screen py-10'>
-        <div className=' flex items-center space-x-4 font-semibold text-[#ffba00]'>
+        <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
           <PiStudentFill size={35} />
           <div className=' text-3xl '>นักเรียน</div>
         </div>
 
-        <div className=' mt-24 flex flex-row justify-center text-xl text-white md:text-3xl'>
+        {/* <div className=' my-24 flex flex-row justify-center text-xl text-white md:text-3xl'>
           สวัสดีค่ะ น้อง ๆ สนใจเรื่องไหนคลิกเลยค่ะ !
-        </div>
+        </div> */}
 
-        <div className=' mt-16 flex  flex-row items-center justify-center space-x-28'>
+        <div className='mt-36 flex  flex-row items-center justify-center space-x-28'>
           <div className='flex flex-col space-y-6'>
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
               Podcast ฟัง พูด อ่าน กิน
             </div>
-            <div className=' flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-2 border-black bg-white p-8 hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
+            <div className=' flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-4 border-black bg-white p-8 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
               <Link href='/podcast'>
                 <FaPodcast size={100} />
               </Link>
@@ -37,12 +37,8 @@ const Student = () => {
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
               TV Program
             </div>
-            <div className='flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-2 border-black bg-white p-8 hover:border-[#ffba00] hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
-              <Link
-                href='https://line.me/R/ti/p/@deschooling?from=page'
-                passHref
-                target='_blank'
-              >
+            <div className='flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-4 border-black bg-white p-8 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
+              <Link href='https://www.altv.tv/home' passHref target='_blank'>
                 <Image
                   src='/images/logoaltv.png'
                   alt='Picture of the author'

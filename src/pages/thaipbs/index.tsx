b31f93ff309a4575f10d3662a325049fca65c152
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-08-14 00:09:16 +0700

    fix : create bg

--- src/pages/thaipbs/index.tsx
+++ src/pages/thaipbs/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '@/components/layout/Layout';
 import Head from 'next/head';
 import { RiQuestionnaireFill } from 'react-icons/ri';
+import Image from 'next/image';
+import { LiaSchoolSolid } from 'react-icons/lia';
 
 const ThaiPbs = () => {
   return (
@@ -13,10 +15,48 @@ const ThaiPbs = () => {
           content='Descholling, พื้นที่การเรียนรู้, ห้องเรียนข้ามเส้น'
         />
       </Head>
-      <div className='h-screen py-10'>
-        <div className=' flex items-center justify-center space-x-4 font-semibold text-[#ffba00]'>
-          <div className='text-3xl'> Deschooling คือ_ </div>
-          {/* <RiQuestionnaireFill size={35} /> */}
+      <div className=' h-full'>
+        <div className='relative'>
+          <div className='absolute left-[30%] top-[8%] mx-24 flex space-x-4 font-semibold text-[#ffba00]'>
+            <LiaSchoolSolid size={35} />
+            <div className=' text-3xl '>ความเป็นมาของ Deschooling</div>
+          </div>
+
+          <div className='absolute left-[35%] top-[34%] w-[40%] text-start  text-lg leading-8'>
+            <p className='indent-8'>
+              เพราะเราเชื่อว่า
+              “ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้” จนเกิดเป็น
+              <span className='mx-2 text-xl font-extrabold text-[#ffba00]'>
+                Deschooling
+              </span>
+              Project โครงการภายใต้ความมุ่งมั่นตั้งใจของ
+            </p>
+            <p className=' text-center '>
+              องค์การกระจายเสียงและแพร่ภาพสาธารณะแห่งประเทศไทย (
+              <span className='text-xl font-extrabold text-[#f05223]'>
+                Thai PBS
+              </span>
+              )
+            </p>
+            <p>
+              ที่อยากจะขับเคลื่อนการศึกษาไทย ด้วยการข้ามเส้นข้อจำกัดเดิม ๆ
+              ผ่านการนำเสนอ นวัตกรรม วิธีการและแนวคิดใหม่ ๆ
+              เพื่อเป็นพื้นที่แลกเปลี่ยนเรียนรู้ร่วมกัน
+              พร้อมกับสร้างความร่วมมือที่ไม่ได้จำกัดแค่บุคลากรทางการศึกษาเท่านั้น
+              แต่ยังรวมถึง ชุมชน หน่วยงานท้องถิ่น ภาครัฐ พ่อแม่ ผู้ปกครอง ครู
+              ผู้อำนวยการโรงเรียน นักเรียน ตลอดจนความร่วมมือจากคนรุ่นใหม่
+              ที่อยากเข้ามามีส่วนร่วมในการพลิกโฉมการศึกษาไทย ผ่านช่องทาง Online
+              และ On Air ตรงกับความต้องการที่หลากหลาย (On Demand)
+              เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
+              และต่อยอดไปสู่การเปลี่ยนแปลงในสังคมวงกว้างต่อไป
+            </p>
+          </div>
+          <Image
+            src='/images/bg-what-deschooling.png'
+            alt='Descholling คืออะไร'
+            width='1500'
+            height='400'
+          />
         </div>
       </div>
     </Layout>

935ff1f5051a9720d204ff5fcf003402bd962caa
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-08-15 17:31:11 +0700

    fix : create about deschooling

--- src/pages/about-deschooling/index.tsx
+++ src/pages/about-deschooling/index.tsx
@@ -6,10 +6,11 @@ import Image from 'next/image';
 import { LiaSchoolSolid } from 'react-icons/lia';
 import { LuBaby } from 'react-icons/lu';
 import { BsFillCalendarHeartFill } from 'react-icons/bs';
+import { Spa } from '@mui/icons-material';
 
 const AboutDeschooling = () => {
   return (
-    <Layout container={false}>
+    <Layout backgroundImage='/images/bg-deschooling.png'>
       <Head>
         <title>ความหมาย : Deschooling</title>
         <meta
@@ -18,25 +19,19 @@ const AboutDeschooling = () => {
         />
       </Head>
 
-      <div className=' flex h-screen w-full flex-row p-10 '>
-        <div className='flex w-1/2 flex-col items-center justify-center  px-6'>
-          <Image
-            src='/images/bg-about-des.png'
-            width={1500}
-            height={1000}
-            alt='Picture of the author'
-          />
-        </div>
-
-        <div className='flex  w-1/2 flex-col items-center justify-center rounded-b-[20%] rounded-t-[20%] border-2 border-dotted border-[#ffba00] px-14'>
-          <div className=' mb-8 flex flex-row space-x-2'>
-            <div className='text-3xl text-[#ffba00]'>
-              ความเป็นมาของ Deschooling
+      <div className=' h-screen px-44 pt-20'>
+        <div className='flex flex-col items-center justify-center '>
+          <div className=' mb-10 flex flex-row space-x-2'>
+            <div className='font-mono text-3xl  font-extrabold'>
+              <span className='text-white'>"ความเป็นมาของ </span>
+              <span className='text-[#ffba00] underline decoration-double decoration-1 underline-offset-4'>
+                Deschooling"
+              </span>
             </div>
           </div>
-          <div className='text-center  text-lg leading-10 text-white'>
+          <div className='text-justify indent-8 text-xl  leading-10 text-white'>
             เราเชื่อว่า “ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้
-            <span className='mx-2 font-mono text-xl font-extrabold text-[#f05223]'>
+            <span className='mx-2 font-mono text-2xl font-extrabold text-[#f05223]'>
               Thai PBS
             </span>
             จึงมีความ ตั้งใจที่อยากจะขับเคลื่อนการศึกษาไทย
@@ -46,62 +41,16 @@ const AboutDeschooling = () => {
             ภาคเอกชน ครู ผู้อำนวยการโรงเรียน ชุมชน ท้องถิ่น พ่อแม่ ผู้ปกครอง
             นักเรียน ตลอดจนความร่วมมือจากประชาชนคนรุ่นใหม่
             ที่อยากเข้ามามีส่วนร่วมในการพลิกโฉมการศึกษาไทย ผ่านช่องทาง Online
-            และ On Air ตรงกับความต้องการที่หลากหลาย (On Demand)
-            เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
+            และ On Air ตรงกับความต้องการที่หลากหลาย (On
+            Demand)เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
             และต่อยอดไปสู่การเปลี่ยนแปลงในสังคมวงกว้าง
             ด้วยการก้าวข้ามทุกปัญหาการศึกษาไทย กับหลักคิดแบบ
-            <span className='mx-2 font-mono text-xl font-extrabold text-[#ffba00]'>
+            <span className='mx-2 font-mono text-2xl font-extrabold text-[#ffba00] '>
               “Deschooling ห้องเรียนข้ามเส้น”
             </span>
           </div>
         </div>
       </div>
-
-      {/* <div className=' h-full'>
-        <div className='relative'>
-          <div className='absolute left-[30%] top-[8%] mx-24 flex space-x-4 font-semibold text-[#ffba00]'>
-            <LiaSchoolSolid size={35} />
-            <div className=' text-3xl'>ความเป็นมาของ Deschooling</div>
-          </div>
-
-          <div className='absolute left-[35%] top-[34%] w-[40%] text-start text-sm leading-8 lg:text-lg'>
-            <p className='indent-8'>
-              เพราะเราเชื่อว่า
-              “ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้” จนเกิดเป็น
-              <span className='mx-2 text-xl font-extrabold text-[#ffba00]'>
-                Deschooling
-              </span>
-              Project โครงการภายใต้ความมุ่งมั่นตั้งใจของ
-            </p>
-            <p className=' text-center '>
-              องค์การกระจายเสียงและแพร่ภาพสาธารณะแห่งประเทศไทย (
-              <span className='text-xl font-extrabold text-[#f05223]'>
-                Thai PBS
-              </span>
-              )
-            </p>
-            <p>
-              ที่อยากจะขับเคลื่อนการศึกษาไทย ด้วยการข้ามเส้นข้อจำกัดเดิม ๆ
-              ผ่านการนำเสนอ นวัตกรรม วิธีการและแนวคิดใหม่ ๆ
-              เพื่อเป็นพื้นที่แลกเปลี่ยนเรียนรู้ร่วมกัน
-              พร้อมกับสร้างความร่วมมือที่ไม่ได้จำกัดแค่บุคลากรทางการศึกษาเท่านั้น
-              แต่ยังรวมถึง ชุมชน หน่วยงานท้องถิ่น ภาครัฐ พ่อแม่ ผู้ปกครอง ครู
-              ผู้อำนวยการโรงเรียน นักเรียน ตลอดจนความร่วมมือจากคนรุ่นใหม่
-              ที่อยากเข้ามามีส่วนร่วมในการพลิกโฉมการศึกษาไทย ผ่านช่องทาง Online
-              และ On Air ตรงกับความต้องการที่หลากหลาย (On Demand)
-              เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
-              และต่อยอดไปสู่การเปลี่ยนแปลงในสังคมวงกว้างต่อไป
-            </p>
-          </div>
-          <Image
-
-            src='/images/bg-what-deschooling.png'
-            alt='Descholling คืออะไร'
-            width='1500'
-            height='400'
-          />
-        </div>
-      </div> */}
     </Layout>
   );
 };

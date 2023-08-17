74fb15b861a0a728587d86fa1e7b672e000b1878
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-08-17 10:34:02 +0700

    fix :font size

--- src/pages/about-deschooling/index.tsx
+++ src/pages/about-deschooling/index.tsx
@@ -15,8 +15,8 @@ const AboutDeschooling = () => {
       </Head>
 
       <div className=' h-screen '>
-        <div className='flex h-[70%] w-full flex-col space-y-14 px-44 py-10'>
-          <div className=' flex flex-row items-center justify-center text-4xl'>
+        <div className='flex h-[70%] w-full flex-col space-y-8 px-44 py-10'>
+          <div className=' flex flex-row items-center justify-center rounded-b-xl  rounded-t-xl  border  border-dotted p-4  text-4xl'>
             <div className=' space-y-4 text-center '>
               <p className=' text-white'>ความเป็นมาของ</p>
               <p className='text-[#ffcc00]'>"Deschooling"</p>
@@ -24,8 +24,8 @@ const AboutDeschooling = () => {
           </div>
 
           <div className='flex flex-row items-center justify-center '>
-            <div className=' rounded-b-xl  rounded-t-xl  border-2 border-dotted  p-4 text-xl leading-10 text-white'>
-              <p className='text-justify indent-10'>
+            <div className=' text-xl leading-10 text-white'>
+              <p className='text-center indent-10'>
                 เราเชื่อว่า “ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้
                 <span className='ml-4 mr-2 font-mono text-xl font-extrabold text-[#f05223]'>
                   Thai PBS
@@ -37,20 +37,13 @@ const AboutDeschooling = () => {
                 ภาคเอกชน ครู ผู้อำนวยการโรงเรียน ชุมชน ท้องถิ่น พ่อแม่ ผู้ปกครอง
                 นักเรียน ตลอดจนความร่วมมือจากประชาชนคนรุ่นใหม่
                 ที่อยากเข้ามามีส่วนร่วมในการพลิกโฉมการศึกษาไทย ผ่านช่องทาง
-                <span className='mx-2 font-mono text-xl font-extrabold text-[#73fa61]'>
-                  Online
-                </span>
-                และ
-                <span className='mx-2 font-mono text-xl font-extrabold text-[#73fa61]'>
-                  On Air
-                </span>
-                ตรงกับความต้องการที่หลากหลาย (On Demand)
+                Online และ On Air ตรงกับความต้องการที่หลากหลาย (On Demand)
                 เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
                 และต่อยอดไปสู่การเปลี่ยนแปลงในสังคมวงกว้าง
                 ด้วยการก้าวข้ามทุกปัญหาการศึกษาไทยกับหลักคิดแบบ
               </p>
 
-              <p className='mx-2 mt-4 text-center font-mono text-2xl font-extrabold text-[#ffcc00] underline underline-offset-2'>
+              <p className='mx-2 mt-4 text-center font-mono text-2xl font-extrabold text-[#ffcc00] '>
                 “Deschooling ห้องเรียนข้ามเส้น”
               </p>
             </div>

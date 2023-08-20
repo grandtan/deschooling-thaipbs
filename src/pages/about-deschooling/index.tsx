1a621d2eacdf3bc840fb7ed0b3c9ac2a228e8c2e
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-08-20 16:47:21 +0700

    fix : text nowrap

--- src/pages/about-deschooling/index.tsx
+++ src/pages/about-deschooling/index.tsx
@@ -15,21 +15,18 @@ const AboutDeschooling = () => {
       </Head>
 
       <div className=' h-screen'>
-        <div className='flex h-[70%] w-full flex-col space-y-8 px-28 pt-20'>
-          <div className='flex flex-row items-center  justify-center'>
-            <div className='text-start text-2xl leading-10 text-white'>
-              <p className='block indent-10'>
-                เราเชื่อว่า “ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้
-                <span className='ml-4 mr-2 text-2xl font-extrabold text-[#f05223]'>
-                  Thai PBS
-                </span>
-                จึงมีความตั้งใจที่อยากจะขับเคลื่อนการศึกษาไทย
-                ด้วยการข้ามข้อจำกัดเดิม ๆ ผ่านการนำเสนอ นวัตกรรม
-                วิธีการและแนวคิดใหม่ ๆ เพื่อเป็นพื้นที่แลกเปลี่ยนเรียนรู้ร่วมกัน
+        <div className='flex h-[70%] w-full flex-col space-y-8 pt-20'>
+          <div className='flex  flex-row items-center justify-center'>
+            <div className='w-[500px] text-start text-2xl leading-10 text-white md:w-[1100px] md:text-justify '>
+              <p className='indent-10'>
+                เราเชื่อว่า "ทุกคนเป็นส่วนหนึ่งในการเปลี่ยนแปลงการศึกษาไทยได้"
+                Thai PBS จึงมีความตั้งใจที่อยากจะขับเคลื่อนการศึกษาไทย
+                ด้วยการข้ามข้อจำกัดเดิม ๆ ผ่านการนำเสนอ นวัตกรรม วิธีการ
+                และแนวคิดใหม่ ๆ เพื่อเป็นพื้นที่แลกเปลี่ยนเรียนรู้ร่วมกัน
                 พร้อมกับสร้าง
               </p>
 
-              {/* <p className=' indent-10'>
+              <p className=' indent-10  '>
                 ความร่วมมือจากบุคลากรหลากหลายภาคส่วนทั้งหน่วยงานภาครัฐ ภาคเอกชน
                 ครู ผู้อำนวยการโรงเรียน ชุมชน ท้องถิ่น พ่อแม่ ผู้ปกครอง นักเรียน
                 ตลอดจนความร่วมมือจากประชาชนคนรุ่นใหม่
@@ -38,7 +35,7 @@ const AboutDeschooling = () => {
                 เพื่อให้เกิดการเข้าถึงกลุ่มเป้าหมาย ได้ทุกที่ ทุกเวลา
                 และต่อยอดไปสู่การเปลี่ยนแปลงในสังคมวงกว้าง
                 ด้วยการก้าวข้ามทุกปัญหาการศึกษาไทยกับแนวคิดแบบ
-              </p> */}
+              </p>
               <p className='mx-2 mt-12  text-center  text-4xl font-extrabold text-[#ffcc00] '>
                 “Deschooling ห้องเรียนข้ามเส้น”
               </p>

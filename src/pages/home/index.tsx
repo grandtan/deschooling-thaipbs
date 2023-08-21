2e0bef603b67a1bb041691e78d328a91f3847953
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-08-21 22:06:45 +0700

    fix : icon align

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { TbZoomQuestion } from 'react-icons/tb';
 import { Slide } from 'react-slideshow-image';
-
+import { Grid } from 'antd';
 import 'react-slideshow-image/dist/styles.css';
 
 import Layout from '@/components/layout/Layout';
@@ -14,8 +14,12 @@ import GirlHome from '@/icon/GirlHome';
 import LocalGovermentHome from '@/icon/LocalGovermentHome';
 import ParentHome from '@/icon/ParentHome';
 
+const { useBreakpoint } = Grid;
 const Home = () => {
+  const screens = useBreakpoint();
   const router = useRouter();
+
+  console.log('screens :', screens);
   return (
     <Layout container={false}>
       <Head>
@@ -26,17 +30,19 @@ const Home = () => {
         />
       </Head>
 
-      <div className=''>
-        <Slide duration={2000}>
-          <div
-            className='each-slide-effect flex cursor-pointer flex-row justify-center'
-            onClick={() => router.push('/about-deschooling')}
-          >
-            <div className=' flex flex-row space-x-8 text-8xl'>
-              <div className=' font-bold text-[#ffcc00]'>Deschooling</div>
-              <div className=' text-white'>คืออะไร_</div>
-              <div>
-                <TbZoomQuestion color='white' size={70} />
+      <div className=' w-full '>
+        <Slide duration={30000} arrows={!screens.xs}>
+          <div className='each-slide-effect flex  flex-row justify-center '>
+            <div className=' flex w-full  space-x-2 text-3xl sm:text-5xl md:space-x-8 md:text-6xl lg:text-7xl xl:text-8xl'>
+              <div
+                className='flex w-full cursor-pointer justify-center'
+                onClick={() => router.push('/about-deschooling')}
+              >
+                <div className=' font-bold text-[#ffcc00]'>Deschooling</div>
+                <div className=' text-white'>คืออะไร_</div>
+                <div>
+                  <TbZoomQuestion color='white' />
+                </div>
               </div>
             </div>
           </div>
@@ -51,19 +57,17 @@ const Home = () => {
               height: '100%',
             }}
           >
-            <div className=' relative h-full w-full '>
-              <div className=' absolute inset-x-0 bottom-0 grid grid-cols-5  '>
-                <div className='absolute bottom-0 col-start-1'>
-                  <div className='absolute left-[110px] top-[-10px] text-xl text-white '>
-                    นักเรียน
-                  </div>
+            <div className='relative '>
+              <div className='grid  grid-cols-5 pt-[400px]  '>
+                <div className='flex flex-col items-center'>
+                  <div className='mb-2 text-xl text-white'>นักเรียน</div>
                   <BabyHome2
-                    className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
+                    className='cursor-pointer duration-700 hover:-translate-y-2 hover:ease-in-out sm:hover:-translate-y-4'
                     onClick={() => router.push('/student')}
                   />
                 </div>
-                <div className='absolute  bottom-4 col-start-2 '>
-                  <div className='absolute left-[110px] top-[-30px] text-xl text-white'>
+                <div className=' flex flex-col items-center'>
+                  <div className='mb-2  mr-8 text-center  text-xl text-white'>
                     รัฐบาล
                   </div>
                   <GirlHome
@@ -72,8 +76,8 @@ const Home = () => {
                   />
                 </div>
 
-                <div className='absolute bottom-2 left-[-70px] col-start-3 '>
-                  <div className='absolute left-[120px] top-[-30px] text-xl text-white '>
+                <div className=' flex  flex-col items-center'>
+                  <div className='mb-2 text-xl text-white'>
                     ครูและผู้อำนวยการ
                   </div>
                   <Image
@@ -81,28 +85,32 @@ const Home = () => {
                     width={350}
                     height={350}
                     alt='local-government'
-                    className=' cursor-pointer  duration-700 hover:-translate-y-4 hover:ease-in-out'
+                    className=' cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/teachers')}
                   />
                 </div>
 
-                <div className='absolute bottom-0 left-[-30px]  col-start-4'>
-                  <div className='absolute left-[70px] top-[-30px]  text-xl text-white'>
+                <div className=' flex flex-col items-center'>
+                  <div className='mb-2 text-xl text-white'>
                     พ่อแม่และผู้ปกครอง
                   </div>
                   <ParentHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/parents')}
+                    width={230}
+                    height={310}
                   />
                 </div>
 
-                <div className='absolute bottom-2 left-[-80px] col-start-5'>
-                  <div className='absolute left-[100px] top-[220px] text-xl text-white'>
+                <div className='  flex flex-col items-center'>
+                  <div className='mb-2 text-xl text-white'>
                     หน่วยงานท้องถิ่น
                   </div>
                   <LocalGovermentHome
                     className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out'
                     onClick={() => router.push('/local-government')}
+                    width={230}
+                    height={310}
                   />
                 </div>
               </div>

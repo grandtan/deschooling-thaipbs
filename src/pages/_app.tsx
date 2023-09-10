bd2cfb84d09d5fdcd019320a70722b11e3abc56f
A667202 A667202 <wetchayan.s@arise.tech>
2023-09-10 18:53:35 +0700

    fix: app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '@/styles/globals.css';
 import '@/styles/colors.css';
@@ -7,6 +8,19 @@ import { LoadingProvider } from '@/context/loadingContext';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LoadingProvider>
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=GTM-5GVNLZRH'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GTM-5GVNLZRH');
+        `}
+      </Script>
       <Component {...pageProps} />
     </LoadingProvider>
   );

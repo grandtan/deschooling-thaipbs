cc312469a7118093082af11a3a042d698d5fb772
A667202 A667202 <wetchayan.s@arise.tech>
2023-09-17 15:21:01 +0700

    fix: gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,17 @@
 import { AppProps } from 'next/app';
-import Script from 'next/script';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 import '@/styles/globals.css';
 import '@/styles/colors.css';
 
 import { LoadingProvider } from '@/context/loadingContext';
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5GVNLZRH' });
+  }, []);
   return (
     <LoadingProvider>
-      <Script
-        src='https://www.googletagmanager.com/gtag/js?id=GTM-5GVNLZRH'
-        strategy='afterInteractive'
-      />
-      <Script
-        id='gtag-init'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-GTM-5GVNLZRH', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-GTM-5GVNLZRH');
-        `}
-      </Script>
       <Component {...pageProps} />
     </LoadingProvider>
   );

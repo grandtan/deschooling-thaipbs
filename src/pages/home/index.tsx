878e272a2166afd8404f34dd2e959a07d404596a
A667202 A667202 <wetchayan.s@arise.tech>
2023-10-06 13:01:04 +0700

    fix: time

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Grid, Row } from 'antd';
+import { Grid } from 'antd';
 import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -31,7 +31,7 @@ const Home = () => {
       </Head>
 
       <div className=' w-full  '>
-        <Slide duration={2000} arrows={!screens.xs}>
+        <Slide duration={4000} arrows={!screens.xs}>
           <div className='each-slide-effect flex  flex-row justify-center '>
             <div className=' flex w-full  space-x-2 text-3xl sm:text-5xl md:space-x-8 md:text-5xl lg:text-7xl xl:text-8xl'>
               <div
@@ -125,90 +125,6 @@ const Home = () => {
                 </div>
               </div>
             </div>
-
-            {/* <Row
-              gutter={[16, 16]}
-              style={{
-                width: '100%',
-                paddingTop: 600,
-                display: 'flex',
-                justifyContent: 'center',
-                overflowY: 'auto',
-              }}
-            >
-              <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-                <div className='flex  flex-col items-center'>
-                  <div className='mb-2 text-center text-xl text-white'>
-                    นักเรียน
-                  </div>
-
-                  <BabyHome2
-                    className='cursor-pointer duration-700 hover:-translate-y-2 hover:ease-in-out  sm:hover:-translate-y-4'
-                    onClick={() => router.push('/student')}
-                  />
-                </div>
-              </Col>
-
-              <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-                <div className=' flex flex-col items-center'>
-                  <div className='mb-2  mr-8 text-center  text-xl text-white'>
-                    รัฐบาล
-                  </div>
-
-                  <GirlHome
-                    className='cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out '
-                    onClick={() => router.push('/government')}
-                  />
-                </div>
-              </Col>
-
-              <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-                <div className=' flex  flex-col items-center'>
-                  <div className='mb-2 text-xl text-white'>
-                    ครูและผู้อำนวยการ
-                  </div>
-
-                  <Image
-                    src='/images/icon-teacher.png'
-                    width={450}
-                    height={450}
-                    alt='local-government'
-                    className=' cursor-pointer duration-700 hover:-translate-y-4  hover:ease-in-out'
-                    onClick={() => router.push('/teachers')}
-                  />
-                </div>
-              </Col>
-
-              <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-                <div className=' flex flex-col items-center'>
-                  <div className='mb-2 text-xl text-white'>
-                    พ่อแม่และผู้ปกครอง
-                  </div>
-
-                  <ParentHome
-                    className=' cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out '
-                    onClick={() => router.push('/parents')}
-                    width={230}
-                    height={310}
-                  />
-                </div>
-              </Col>
-
-              <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-                <div className='  flex flex-col items-center '>
-                  <div className='mb-2 text-xl text-white'>
-                    หน่วยงานท้องถิ่น
-                  </div>
-
-                  <LocalGovermentHome
-                    className=' cursor-pointer duration-700 hover:-translate-y-4 hover:ease-in-out '
-                    onClick={() => router.push('/local-government')}
-                    width={230}
-                    height={310}
-                  />
-                </div>
-              </Col>
-            </Row> */}
           </div>
         </Slide>
       </div>

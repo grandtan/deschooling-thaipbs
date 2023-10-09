85aed1d423a8d471568288cce6715276978c1d05
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2023-10-09 22:40:05 +0700

    fix : upadte layout

--- src/pages/parents/index.tsx
+++ src/pages/parents/index.tsx
@@ -26,34 +26,34 @@ const Parents = () => {
           <div className=' text-3xl '> พ่อแม่และผู้ปกครอง</div>
         </div>
 
-        <div className='mt-8 flex flex-col items-center  justify-center  gap-8  md:mt-36 md:flex-row   md:space-x-10'>
-          <div className='flex flex-col space-y-6'>
+        <div className='mt-8 flex flex-col items-center  justify-center gap-4 md:mt-36  md:flex-row md:gap-10   '>
+          <div className='flex flex-col space-y-4 md:space-y-6'>
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
               Home-Based Learning
             </div>
-            <div className=' flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-4 border-black bg-white p-8 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
+            <div className=' flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
               <Link href='/parent'>
                 <MdComputer size={100} />
               </Link>
             </div>
           </div>
 
-          <div className=' flex-col space-y-6'>
+          <div className=' flex-col space-y-4 md:space-y-6'>
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
               Podcast
             </div>
-            <div className=' flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-4 border-black bg-white p-8 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
+            <div className=' flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
               <Link href='/podcast'>
                 <FaPodcast size={100} />
               </Link>
             </div>
           </div>
 
-          <div className=' flex-col space-y-6'>
+          <div className=' flex-col space-y-4 md:space-y-6'>
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
               TV Program
             </div>
-            <div className='flex h-[100px] w-[200px] items-center justify-center  rounded-[50px] border-4 border-black bg-white p-8 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:p-0'>
+            <div className='flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
               <Link href='https://www.altv.tv/home' passHref target='_blank'>
                 <Image
                   src='/images/logoaltv.png'

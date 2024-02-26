70debe07b09045d7745995320f95d32f40360175
grandtan_true <Wetchayan_Sor@truecorp.co.th>
2024-02-26 16:48:28 +0700

    feat: Create small-school and classroom

--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -50,6 +50,22 @@ const TeacherAll = () => {
               </Link>
             </div>
           </div>
+
+          <div className=' flex-col space-y-2 md:space-y-6'>
+            <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
+              TV Program
+            </div>
+            <div className='flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
+              <Link href='https://www.altv.tv/home' passHref target='_blank'>
+                <Image
+                  src='/images/logoaltv.png'
+                  alt='Picture of the author'
+                  width={200}
+                  height={200}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
 
         <div className='flex flex-col items-center justify-center  gap-6 pt-4 md:flex-row  md:gap-10   md:pt-8'>
@@ -64,18 +80,24 @@ const TeacherAll = () => {
             </div>
           </div>
 
-          <div className=' flex-col space-y-2 md:space-y-6'>
+          <div className='flex flex-col space-y-2 md:space-y-6'>
             <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
-              TV Program
+              โรงเรียนขนาดเล็กต้นแบบ
             </div>
-            <div className='flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
-              <Link href='https://www.altv.tv/home' passHref target='_blank'>
-                <Image
-                  src='/images/logoaltv.png'
-                  alt='Picture of the author'
-                  width={200}
-                  height={200}
-                />
+            <div className=' flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
+              <Link href='/small-school'>
+                <MdComputer size={100} />
+              </Link>
+            </div>
+          </div>
+
+          <div className='flex flex-col space-y-2 md:space-y-6'>
+            <div className=' text-center text-xl text-[#ffba00] md:text-2xl'>
+              ห้องเรียนบูรณาการ
+            </div>
+            <div className=' flex h-[100px] w-[200px] items-center justify-center rounded-full border-4 border-black bg-white p-14 hover:border-white hover:bg-[#ffba00] hover:text-white md:h-[150px] md:w-[260px] md:rounded-[50px] md:p-0'>
+              <Link href='/classroom'>
+                <MdComputer size={100} />
               </Link>
             </div>
           </div>

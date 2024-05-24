483c3e9fa4a08853850d0046ee8ea68db8225875
A667202 A667202 <wetchayan.s@arise.tech>
2024-05-24 20:25:22 +0700

    feat: Add Google Tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,46 @@
 import { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
 import TagManager from 'react-gtm-module';
 
 import '@/styles/globals.css';
 import '@/styles/colors.css';
 
 import { LoadingProvider } from '@/context/loadingContext';
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: 'GTM-5GVNLZRH' });
   }, []);
+
   return (
-    <LoadingProvider>
-      <Component {...pageProps} />
-    </LoadingProvider>
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9C3MRPK6YN`}
+        />
+        <Script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9C3MRPK6YN', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <LoadingProvider>
+        <Component {...pageProps} />
+      </LoadingProvider>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,33 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
         <link rel='icon' href='/favicon/favicon.ico' sizes='16x16' />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5GVNLZRH');
+            `,
+          }}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5GVNLZRH'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>

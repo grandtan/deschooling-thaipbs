891a404112dfd2a2cf461f09c343095dfc504f35
prapapan <prapapan@mesaverde.app>
2025-02-02 18:47:10 +0700

    feat: Create feedback menu

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,25 +1,33 @@
 import Head from 'next/head';
 import React from 'react';
+import { IoDocumentText } from 'react-icons/io5';
 
-import Document from '@/components/layout/components/document';
-import PreviewClip from '@/components/layout/components/previewClip';
 import Layout from '@/components/layout/Layout';
-
-const PLAYLIST_ID = 'PLeZwwnM5meV-muR4SgMOhWgXV18n_Yy1l';
 
 const Feedback = () => {
   return (
     <Layout container={false}>
       <Head>
-        <title>Teacher Classroom : deschooling</title>
+        <title>รับฟังเสียงสะท้อน : deschooling</title>
         <meta
           name='keywords'
           content='Deschooling, พื้นที่การเรียนรู้, ห้องเรียนข้ามเส้น'
         />
       </Head>
 
-      <div className='flex h-full w-full flex-col border-t-4 border-yellow-500 bg-white'>
-        <Document title='บทความ : รับฟังเสียงสะท้อน' titleFile='' />
+      <div className='flex h-screen w-full flex-col border-t-4 border-yellow-400 bg-white pb-10'>
+        <div className='bg-[#2b2a2a]'>
+          <div className='mx-4 py-14 md:mx-24'>
+            <div className='flex flex-col space-y-6 font-semibold'>
+              <div className='flex justify-center space-x-4'>
+                <IoDocumentText size={35} className='text-[#ffba00]' />
+                <div className='text-3xl font-bold text-white'>
+                  <div>บทความ : รับฟังเสียงสะท้อน</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );

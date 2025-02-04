0014f46aab1793b7945bf3ca74ffd21a336f77b6
prapapan <prapapan@mesaverde.app>
2025-02-04 12:36:40 +0700

    feat: Create article konkaen

--- src/pages/cooperation/index.tsx
+++ src/pages/cooperation/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import React from 'react';
 
 import Layout from '@/components/layout/Layout';
+import Konkaen from '@/pages/cooperation/component/konkaen';
+import { IoDocumentText } from 'react-icons/io5';
 
 const Cooperation = () => {
   return (
@@ -15,7 +17,22 @@ const Cooperation = () => {
         />
       </Head>
 
-      <div>ความร่วมมือ</div>
+      <div className='flex h-full w-full flex-col border-t-4 border-yellow-400 bg-white pb-10'>
+        <div className='bg-[#2b2a2a]'>
+          <div className='mx-4 py-14 md:mx-24'>
+            <div className='flex flex-col space-y-6 font-semibold'>
+              <div className='flex justify-center space-x-4'>
+                <IoDocumentText size={35} className='text-[#ffba00]' />
+                <div className='text-3xl font-bold text-white'>
+                  <div>บทความสรุปกิจกรรม</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Konkaen />
+      </div>
     </Layout>
   );
 };
